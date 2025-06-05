9e0a465c8308fb05a2161aecbb150001faa72040
Kostiantyn Yemets <61251118+kyemets@users.noreply.github.com>
2025-06-05 23:43:23 +0200

    Create healthcheck.py

---
+++ storybot/healthcheck.py
@@ -0,0 +1,12 @@
+# storybot/healthcheck.py
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HealthCheckHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+def start_health_server():
+    server = HTTPServer(('0.0.0.0', 8000), HealthCheckHandler)
+    server.serve_forever()
